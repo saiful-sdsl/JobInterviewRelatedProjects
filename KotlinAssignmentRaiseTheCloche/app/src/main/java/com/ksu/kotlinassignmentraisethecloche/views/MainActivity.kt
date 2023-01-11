41c31401290b07e71e31dbd2f46f543c8b5f7ee4
Md Saiful Islam <MdSaiful.Islam41@T-Mobile.com>
2023-01-11 10:03:24 -0500

    Restructure, splash screen, and app icons.
    - Restructure packages and files,
    - Added API provided splash screen
    - Added App Icon

--- KotlinAssignmentRaiseTheCloche/app/src/main/java/com/ksu/kotlinassignmentraisethecloche/views/MainActivity.kt
+++ KotlinAssignmentRaiseTheCloche/app/src/main/java/com/ksu/kotlinassignmentraisethecloche/views/MainActivity.kt
@@ -1,7 +1,15 @@
-package com.ksu.kotlinassignmentraisethecloche
+package com.ksu.kotlinassignmentraisethecloche.views
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ksu.kotlinassignmentraisethecloche.R
 import com.ksu.kotlinassignmentraisethecloche.billings.Billings
 import com.ksu.kotlinassignmentraisethecloche.course.Menu
 import com.ksu.kotlinassignmentraisethecloche.course.MainMenu
@@ -12,11 +20,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //viewSplashScreen()
+        viewSplashScreenWithDelay()
+
         val binding = ActivityMainLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnCalculateBill.setOnClickListener {
             processOrder()
         }
+    }
+
+    private fun viewSplashScreenWithDelay() {
+         val splashScreen = installSplashScreen()
+        setDelayOnSplashScreen(splashScreen)
+    }
+
+    private fun viewSplashScreen() {
+       installSplashScreen()
+    }
+
+    private fun setDelayOnSplashScreen(splashScreen: SplashScreen) {
+        var keepSplashOnScreen = true
+        val delay = 2000L
+        splashScreen.setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
     }
 
     private fun processOrder() {
